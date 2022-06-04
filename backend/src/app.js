d881ce8d8bc2bf0027d0d0fab0eac8ccbb0ae019
dat <you@example.com>
2022-06-04 13:44:24 +0700

    create server with socket

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+const logger = require('morgan');
+
+dotenv.config({path: path.resolve(__dirname, './config/.env')});
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(logger("dev"));
+app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+
+
+module.exports = app;
