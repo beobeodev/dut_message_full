2749070c7e75e0454b0026452d7472c9e9c24fdd
dat <you@example.com>
2022-06-16 22:26:20 +0700

    handler event add friend

--- backend/src/socketUtils/socket_controller.js
+++ backend/src/socketUtils/socket_controller.js
@@ -31,6 +31,21 @@ class SocketController {
       const F_request = await this._userService.createFriendRequest(data.fromId, data.toId);
       io.to(`${this._socketRepo.getSocketIdByUserId(data.toId)}`).emit(socketConsts.EVENT_RECEIVE_FRIEND_REQUEST, F_request);
     }
+
+    async addFriendHandler(socket, io, data) {
+      await this._userService.addFriend(data.fromId, data.toId);
+      const F_request = await this._userService.getFriendRequest(data.fromId, data.toId);
+      const existConver = await this._messageService.checkExistConver([data.fromId, data.toId]);
+      let conver;
+      if(existConver) {
+          conver = existConver;
+      } else {
+          conver = await this._messageService.createConversation([data.fromId, data.toId]);
+      }
+      await this._userService.removeFriendRequest(F_request._id);
+      io.to(`${this._socketRepo.getSocketIdByUserId(data.fromId)}`).emit(socketConsts.EVENT_NOTIFY_ACCEPT_FRIEND, {'infoFriend': F_request.to, 'conver': conver});
+      io.to(`${this._socketRepo.getSocketIdByUserId(data.toId)}`).emit(socketConsts.EVENT_NOTIFY_ACCEPT_FRIEND, {'infoFriend': F_request.from, 'conver': conver});
+    }
 }
 
 module.exports = SocketController;
