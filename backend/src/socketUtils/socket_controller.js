253b42e5168396c86e4d6d723a26a9043fe92814
dat <you@example.com>
2022-06-16 22:24:58 +0700

    handler conver message

--- backend/src/socketUtils/socket_controller.js
+++ backend/src/socketUtils/socket_controller.js
@@ -6,6 +6,16 @@ class SocketController {
         this._messageService = messageService;
         this._socketRepo = socketRepo;
     }
+
+    async converMessageHandler(socket, io, data) {
+      let message;
+      message = await this._messageService.addMessageToConver(data.converId, {author: data.fromUserId, content: data.content, message_type: data.message_type});
+      let result = {
+          converId: data.converId,
+          message: message,
+      }
+      io.to(`${this._socketRepo.getSocketIdByUserId(data.toUserId)}`).to(`${this._socketRepo.getSocketIdByUserId(data.fromUserId)}`).emit(socketConsts.EVENT_RECEIVE_CONVER_MESSAGE, result);
+  }
 }
 
 module.exports = SocketController;
