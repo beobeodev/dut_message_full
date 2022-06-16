e1013617486dee79b5938d3fc991661d42608e7c
dat <you@example.com>
2022-06-16 22:25:19 +0700

    handler room  message

--- backend/src/socketUtils/socket_controller.js
+++ backend/src/socketUtils/socket_controller.js
@@ -15,7 +15,17 @@ class SocketController {
           message: message,
       }
       io.to(`${this._socketRepo.getSocketIdByUserId(data.toUserId)}`).to(`${this._socketRepo.getSocketIdByUserId(data.fromUserId)}`).emit(socketConsts.EVENT_RECEIVE_CONVER_MESSAGE, result);
-  }
+    }
+
+    async  roomMessageHandler(socket, io, data) {
+      let message;
+      message = await this._messageService.addMessageToRoom(data.roomId, {author: data.fromUserId, content: data.content, message_type: data.message_type});
+      let result = {
+          roomId: data.roomId,
+          message: message,
+      }
+      io.to(data.roomId).emit(socketConsts.EVENT_RECEIVE_ROOM_MESSAGE, result);
+    }
 }
 
 module.exports = SocketController;
