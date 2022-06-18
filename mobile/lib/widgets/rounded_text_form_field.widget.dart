bc3400087cab12fa909627f0eba4ca346c8e0a28
beobiebom <beobiebom@gmail.com>
2022-06-18 23:32:11 +0700

    feat(general_widgets): create general widgets to reuseable

--- mobile/lib/widgets/rounded_text_form_field.widget.dart
+++ mobile/lib/widgets/rounded_text_form_field.widget.dart
@@ -1,42 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/core/constants/font_family.dart';
 import 'package:mobile/core/theme/palette.dart';
+import 'package:mobile/core/theme/text_styles.dart';
 
 class RoundedTextFormField extends StatelessWidget {
-  final TextEditingController textController;
-  final String? Function(String?)? validator;
-  final bool obscureText;
+  final TextEditingController? textController;
+
+  final bool isObscure;
+  final bool readOnly;
+
   final String? hintText;
   final String? errorText;
+
   final double borderRadius;
+
   final Color borderColor;
-  final Widget? suffixIconWidget;
-  final Widget? prefixIconWidget;
+  final Color fillColor;
+
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+
+  final TextInputType? keyboardType;
+
+  final String? Function(String?)? validator;
+  final void Function()? onTap;
+  final void Function(String)? onChanged;
 
   const RoundedTextFormField({
     Key? key,
-    required this.textController,
-    this.validator,
-    this.obscureText = false,
+    this.textController,
+    this.isObscure = false,
+    this.readOnly = false,
     this.hintText,
     this.errorText,
     this.borderRadius = 6,
     this.borderColor = Palette.blue200,
-    this.suffixIconWidget,
-    this.prefixIconWidget,
+    this.fillColor = Colors.white,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.keyboardType,
+    this.validator,
+    this.onTap,
+    this.onChanged,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      // focusNode: focusNode,
       controller: textController,
       textInputAction: TextInputAction.done,
-      onChanged: (val) {},
-      onTap: () {},
+      onChanged: onChanged,
+      onTap: onTap,
       validator: validator,
-      obscureText: obscureText,
+      obscureText: isObscure,
+      readOnly: readOnly,
+      keyboardType: keyboardType,
+      style: const TextStyle(
+        fontFamily: FontFamily.fontNunito,
+        color: Palette.zodiacBlue,
+        fontSize: 14,
+      ),
       decoration: InputDecoration(
         hintText: hintText,
+        hintStyle: const TextStyle(
+          fontFamily: FontFamily.fontNunito,
+          color: Palette.gray300,
+          fontSize: 14,
+        ),
         focusedErrorBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(borderRadius),
           borderSide: BorderSide(
@@ -61,17 +92,15 @@ class RoundedTextFormField extends StatelessWidget {
             color: Colors.red,
           ),
         ),
-        hintStyle: const TextStyle(
-          fontFamily: FontFamily.fontNunito,
-          color: Palette.white300,
-          fontSize: 14,
-        ),
-        fillColor: Colors.white,
+
+        // errorStyle: ,
+        fillColor: fillColor,
         filled: true,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 14),
-        suffixIcon: suffixIconWidget,
-        prefixIcon: prefixIconWidget,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+        suffixIcon: suffixIcon,
+        prefixIcon: prefixIcon,
         errorText: errorText == '' || errorText == null ? null : errorText,
+        errorStyle: TextStyles.mediumRegularText.copyWith(color: Colors.red),
       ),
     );
   }
