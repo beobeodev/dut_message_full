a084d4882ef550b261f1cf019d508b12090c5d52
beobiebom <beobiebom@gmail.com>
2022-06-19 20:46:01 +0700

    refactor(di): pass datasource to di file repository

--- mobile/lib/injector.dart
+++ mobile/lib/injector.dart
@@ -1,8 +1,9 @@
+import 'package:mobile/data/datasources/remote/file.datasource.dart';
 import 'package:mobile/data/repositories/auth.repository.dart';
 import 'package:mobile/data/repositories/conversation.repository.dart';
-import 'package:mobile/data/repositories/firebase_repository.dart';
+import 'package:mobile/data/repositories/file_repository.dart';
 import 'package:mobile/data/repositories/hive_local.repository.dart';
-import 'package:mobile/data/repositories/user_repository.dart';
+import 'package:mobile/data/repositories/user.repository.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt getIt = GetIt.instance;
@@ -10,7 +11,13 @@ final GetIt getIt = GetIt.instance;
 void initDependencies() {
   getIt.registerLazySingleton(() => HiveLocalRepository());
   getIt.registerLazySingleton(() => AuthRepository());
-  getIt.registerLazySingleton(() => FirebaseRepository());
   getIt.registerLazySingleton(() => ConversationRepository());
   getIt.registerLazySingleton(() => UserRepository());
+
+  //* File
+  getIt.registerLazySingleton(() => FileRemoteDataSource());
+  getIt.registerLazySingleton(
+    () =>
+        FileRepository(fileRemoteDataSource: getIt.get<FileRemoteDataSource>()),
+  );
 }
